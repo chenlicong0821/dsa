cc2e856369fcabd14fd00b2ce565c3e4c7429f46
chenlicong <chenlicong@123.com>
2020-03-08 21:54:11 +0800

    mod 235

--- LeetCode/235. Lowest Common Ancestor of a Binary Search Tree/Solution.py
+++ LeetCode/235. Lowest Common Ancestor of a Binary Search Tree/Solution.py
@@ -6,9 +6,19 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def lowestCommonAncestor1(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if p.val < root.val > q.val:
             return self.lowestCommonAncestor(root.left, p, q)
         if p.val > root.val < q.val:
             return self.lowestCommonAncestor(root.right, p, q)
+        return root
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if p.val < root.val > q.val:
+                root = root.left
+            elif p.val > root.val < q.val:
+                root = root.right
+            else:
+                break
         return root

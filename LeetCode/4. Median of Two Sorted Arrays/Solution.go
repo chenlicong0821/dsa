dd80920a7298a3d9543249333c5995c9ac7feddf
chenlicong <chenlicong@123.com>
2021-03-20 20:00:09 +0800

    add LC4

---
+++ LeetCode/4. Median of Two Sorted Arrays/Solution.go
@@ -0,0 +1,56 @@
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	if len(nums1) > len(nums2) {
+		return findMedianSortedArrays(nums2, nums1)
+	}
+	m, n := len(nums1), len(nums2)
+	left, right := 0, m
+	halfLen := (m + n + 1) / 2
+	for left <= right {
+		i := (left + right) / 2
+		j := halfLen - i
+		if i < right && nums1[i] < nums2[j-1] {
+			left = i + 1
+		} else if i > left && nums1[i-1] > nums2[j] {
+			right = i - 1
+		} else {
+			maxLeft := 0
+			if i == 0 {
+				maxLeft = nums2[j-1]
+			} else if j == 0 {
+				maxLeft = nums1[i-1]
+			} else {
+				maxLeft = max(nums1[i-1], nums2[j-1])
+			}
+			if (m+n)%2 == 1 {
+				return float64(maxLeft)
+			}
+
+			minRight := 0
+			if i == m {
+				minRight = nums2[j]
+			} else if j == n {
+				minRight = nums1[i]
+			} else {
+				minRight = min(nums1[i], nums2[j])
+			}
+
+			return float64(maxLeft+minRight) / 2.0
+		}
+	}
+
+	return 0.0
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
